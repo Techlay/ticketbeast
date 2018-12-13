4eb1b4fe2c0f1f6ba2aa377c4bdb6794c90ae94d
Techlay <lytechlay@gmail.com>
2018-12-13 12:38:25 +1100

    Returning order details

--- app/Http/Controllers/ConcertOrderController.php
+++ app/Http/Controllers/ConcertOrderController.php
@@ -37,7 +37,8 @@ class ConcertOrderController extends Controller
                 request('ticket_quantity') * $concert->ticket_price, request('payment_token')
             );
 
-            return response()->json([], 201);
+            return response()->json($order, 201);
+
         } catch (PaymentFailedException $e) {
             $order->cancel();
             return response()->json([], 422);
