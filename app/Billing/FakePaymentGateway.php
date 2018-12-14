370dde931e3695c5b9921b96c28df67fb7c71423
Techlay <lytechlay@gmail.com>
2018-12-14 16:17:14 +1100

    Hooking inot charges

--- app/Billing/FakePaymentGateway.php
+++ app/Billing/FakePaymentGateway.php
@@ -7,6 +7,7 @@ namespace App\Billing;
 class FakePaymentGateway implements PaymentGateway
 {
     private $charges;
+    private $beforeFirstChargeCallback;
 
     /**
      * FakePaymentGateway constructor.
@@ -23,6 +24,10 @@ class FakePaymentGateway implements PaymentGateway
 
     public function charge(int $amount, string $token)
     {
+        if ($this->beforeFirstChargeCallback !== null) {
+            $this->beforeFirstChargeCallback->__invoke($this);
+        }
+
         if ($token !== $this->getValidTestToken()) {
             throw new PaymentFailedException;
         }
@@ -32,5 +37,10 @@ class FakePaymentGateway implements PaymentGateway
     public function totalCharges() : int
     {
         return $this->charges->sum();
+    }
+
+    public function beforeFirstCharge($callback)
+    {
+        $this->beforeFirstChargeCallback = $callback;
     }
 }
