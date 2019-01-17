a4bb22f5f9575bb8303633f0e03b485923c7f021
Techlay <lytechlay@gmail.com>
2019-01-17 21:45:39 +1100

    Refactoring Mocks to Spies

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -29,19 +29,19 @@ class ReservationTest extends TestCase
     /** @test */
     public function reserved_tickets_are_released_when_a_reservation_is_cancelled()
     {
-        $ticket1 = Mockery::mock(Ticket::class);
-        $ticket1->shouldReceive('release')->once();
+        $tickets = collect([
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
 
-        $ticket2 = Mockery::mock(Ticket::class);
-        $ticket2->shouldReceive('release')->once();
-
-        $ticket3 = Mockery::mock(Ticket::class);
-        $ticket3->shouldReceive('release')->once();
-
-        $tickets = collect([$ticket1, $ticket2, $ticket3]);
+        ]);
 
         $reservation = new Reservation($tickets);
 
         $reservation->cancel();
+
+        foreach ($tickets as $ticket) {
+            $ticket->shouldHaveReceived('release');
+        }
     }
 }
