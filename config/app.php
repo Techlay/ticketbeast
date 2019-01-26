2fca8acf8ec456d36d8c2198236694e0e3f69798
Techlay <lytechlay@gmail.com>
2019-01-26 22:41:37 +1100

    Front end preparation

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Zondicons\ZondiconsServiceProvider::class,
 
         /*
          * Application Service Providers...
---
+++ config/zondicons.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'spritesheet_path' => resource_path('icons/sprite.svg'),
+    'inline' => false,
+    'class' => 'zondicon'
+];
