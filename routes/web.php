110686e84e660ae3779071832d8dac47f9be30dd
Techlay <lytechlay@gmail.com>
2019-02-17 14:11:27 +1100

    Add authorization tests for publishing concerts

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::group(['middleware' => 'auth', 'prefix' => 'backstage', 'namespace' => 'B
     Route::post('concerts', 'ConcertController@store');
     Route::get('/concerts/{id}/edit', 'ConcertController@edit')->name('backstage.concerts.edit');
     Route::patch('/concerts/{id}', 'ConcertController@update')->name('backstage.concerts.update');
+    Route::post('/published-concerts', 'PublishedConcertController@store');
 });
 
-Route::post('backstage/published-concerts', 'Backstage\PublishedConcertController@store');
