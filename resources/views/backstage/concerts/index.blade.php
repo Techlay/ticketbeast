9fe4d76780777a309ef3c4d4a1c6d10f710a2e60
Techlay <lytechlay@gmail.com>
2019-02-17 15:55:21 +1100

    Extract a shared backstage layout

--- resources/views/backstage/concerts/index.blade.php
+++ resources/views/backstage/concerts/index.blade.php
@@ -1,24 +1,6 @@
-@extends('layouts.master')
+@extends('layouts.backstage')
 
-@section('body')
-    <header>
-        <nav class="navbar p-xs-y-3">
-            <div class="container">
-                <div class="navbar-content">
-                    <div>
-                        <img src="/img/logo.svg" alt="TicketBeast" style="height: 2.5rem;">
-                    </div>
-                    <div>
-                        <form class="inline-block" action="{{ route('auth.logout') }}" method="POST">
-                            {{ csrf_field() }}
-                            <button type="submit" class="link link-light">Log out</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
-
+@section('backstageContent')
     <div class="bg-light p-xs-y-4 border-b">
         <div class="container">
             <div class="flex-spaced flex-y-center">
@@ -51,8 +33,9 @@
                                         </p>
                                     </div>
                                     <div>
+                                        <a href="#" class="btn btn-sm btn-secondary m-xs-r-2">Manage</a>
                                         <a href="{{ route('concerts.show', $concert) }}"
-                                           class="btn btn-sm btn-secondary">Get Ticket Link</a>
+                                           class="link-brand text-sm wt-medium">Public Link</a>
                                     </div>
                                 </div>
                             </div>
@@ -99,11 +82,4 @@
             </div>
         </div>
     </div>
-
-
-    <footer class="p-xs-y-6 text-light-muted">
-        <div class="container">
-            <p class="text-center">&copy; TicketBeast {{ date('Y') }}</p>
-        </div>
-    </footer>
 @endsection
