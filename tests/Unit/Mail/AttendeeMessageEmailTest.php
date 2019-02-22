9305edbe28f06b0aa70298dd571faae535e6ff89
Techlay <lytechlay@gmail.com>
2019-02-22 23:10:20 +1100

    Mailable testing refresher and demo

---
+++ tests/Unit/Mail/AttendeeMessageEmailTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit\Mail;
+
+use App\AttendeeMessage;
+use App\Mail\AttendeeMessageEmail;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AttendeeMessageEmailTest extends TestCase
+{
+    /** @test */
+    function email_has_the_correct_subject_and_message()
+    {
+        $message = new AttendeeMessage([
+            'subject' => 'My subject',
+            'message' => 'My message',
+        ]);
+        $email = new AttendeeMessageEmail($message);
+
+        $this->assertEquals('My subject', $email->build()->subject);
+        $this->assertEquals('My message', trim($this->render($email)));
+    }
+
+    private function render($mailable)
+    {
+        $mailable->build();
+        return view($mailable->textView, $mailable->buildViewData())->render();
+    }
+}
