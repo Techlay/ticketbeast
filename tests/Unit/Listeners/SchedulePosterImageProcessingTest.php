d2088e462b6e47d3dc78c6505bcf0fc7b50bd3f7
Techlay <lytechlay@gmail.com>
2019-02-24 15:49:39 +1100

    Testing the Event Listener

---
+++ tests/Unit/Listeners/SchedulePosterImageProcessingTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit\Listeners;
+
+use App\Concert;
+use App\Events\ConcertAdded;
+use App\Jobs\ProcessPosterImage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+use Tests\TestCase;
+
+class SchedulePosterImageProcessingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_requires_a_job_to_process_a_poster_image_if_a_poster_image_is_present()
+    {
+        Queue::fake();
+
+        $concert = factory(Concert::class)->create(['poster_image_path' => 'posters/example-poster.png']);
+        $concert->publish();
+
+        ConcertAdded::dispatch($concert);
+
+        Queue::assertPushed(ProcessPosterImage::class, function ($job) use ($concert) {
+            return $job->concert->is($concert);
+        });
+    }
+
+    /** @test */
+    function a_job_is_not_queued_if_a_poster_is_not_present ()
+    {
+        Queue::fake();
+
+        $concert = factory(Concert::class)->create(['poster_image_path' => null]);
+        $concert->publish();
+
+        ConcertAdded::dispatch($concert);
+
+        Queue::assertNotPushed(ProcessPosterImage::class);
+    }
+}
