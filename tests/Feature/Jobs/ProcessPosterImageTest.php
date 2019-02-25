40dc6fd595b5e4a7f672a7efd676738228ffe112
Techlay <lytechlay@gmail.com>
2019-02-25 21:36:39 +1100

    Optimising the image size

--- tests/Feature/Jobs/ProcessPosterImageTest.php
+++ tests/Feature/Jobs/ProcessPosterImageTest.php
@@ -4,10 +4,9 @@ namespace Tests\Feature\Jobs;
 
 use App\Concert;
 use App\Jobs\ProcessPosterImage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProcessPosterImageTest extends TestCase
 {
@@ -27,7 +26,34 @@ class ProcessPosterImageTest extends TestCase
         ProcessPosterImage::dispatch($concert);
 
         $resizeImage = Storage::disk('public')->get('posters/example-poster.png');
-        list($width) = getimagesizefromstring($resizeImage);
+        list($width, $height) = getimagesizefromstring($resizeImage);
         $this->assertEquals(600, $width);
+        $this->assertEquals(776, $height);
+
+        $resizedImageContents = Storage::disk('public')->get('posters/example-poster.png');
+        $controlImageContents = file_get_contents(base_path('tests/__fixtures__/optimized-poster.png'));
+        $this->assertEquals($controlImageContents, $resizedImageContents);
+    }
+
+    /** @test */
+    function it_optimises_the_poster_image()
+    {
+        Storage::fake('public');
+        Storage::disk('public')->put(
+            'posters/example-poster.png',
+            file_get_contents(base_path('tests/__fixtures__/small-unoptimized-poster.png'))
+        );
+        $concert = factory(Concert::class)->create(['poster_image_path' => 'posters/example-poster.png']);
+        $concert->publish();
+
+        ProcessPosterImage::dispatch($concert);
+
+        $optimisedImageSize = Storage::disk('public')->size('posters/example-poster.png');
+        $originalSize = filesize(base_path('tests/__fixtures__/small-unoptimized-poster.png'));
+        $this->assertLessThan($originalSize, $optimisedImageSize);
+
+        $optimisedImageContents = Storage::disk('public')->get('posters/example-poster.png');
+        $controlImageContents = file_get_contents(base_path('tests/__fixtures__/optimized-poster.png'));
+        $this->assertEquals($controlImageContents, $optimisedImageContents);
     }
 }
